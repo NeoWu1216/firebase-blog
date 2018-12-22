4bfc1b32755db9a6e54be2aedc8d6a6a25da2713
NeoWu1216 <neo1216.wu@gmail.com>
2018-12-22 17:10:57 -0500

    Add users.py

---
+++ test/selenium/users.py
@@ -0,0 +1,5 @@
+user1 = {'email':'a@gmail.com', 'password':'123456'}
+user2 = {'email':'aa@gmail.com', 'password':'123456'}
+user3 = {'email':'aaa@gmail.com', 'password':'123456'}
+users = [user1, user2, user3]
+uid = "sJNpAJ2rtTRJ5bUGb7FXWNGBoxI2" # Any existing user id except user2
